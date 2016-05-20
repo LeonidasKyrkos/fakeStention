81e11696739a8ab76bd59f23f67fdaa410d075cf
LeonidasKyrkos <leo.kyrkos@gmail.com>
2016-05-20 16:37:55 +0100

    minor layout edit

--- content.js
+++ content.js
@@ -5,24 +5,22 @@
 	
 	if(day % 2) {
 		
-		var $img = $('img');
-			
-		$img.each(alter);
-		
-		function alter(i,el) {
-			var rand = Math.random();
-			if(rand <= 0.003) {
-				$(el).prop('src','http://assets2.lfcimages.com/uploads/placeholders/splash_crest.png');
-			}
-		}
-		
-		var rand2 = Math.random();
-		
-		if(rand2 <= 0.07) {
+		var $img = $('img');			
+		$img.each(alter);	
+					
+		var rand2 = Math.random();		
+		if(rand2 <= 0.1) {
 			$('body').css({'-webkit-transform' : 'rotate(0.25deg)',
 					'-moz-transform' : 'rotate(0.25deg)',
 					'-ms-transform' : 'rotate(0.25deg)',
 					'transform' : 'rotate(0.25deg)'});
+		}
+	}
+	
+	function alter(i,el) {
+		var rand = Math.random();
+		if(rand <= 0.003) {
+			$(el).prop('src','http://assets2.lfcimages.com/uploads/placeholders/splash_crest.png');
 		}
 	}
 	
